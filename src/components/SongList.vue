5d53b24634d9d2988e56c10b3acfc33bddef7ca8
DrissBouda <boudafon@gmail.com>
2022-10-17 14:04:22 +0200

    Update SongList.vue

--- src/components/SongList.vue
+++ src/components/SongList.vue
@@ -48,7 +48,7 @@ export default {
     "name": "N3icho La Vida",
     "artistName": "Cheb Momo",
     "year": 2020,
-    "src": "./assets/cheb momo.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/cheb momo.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/Cheb%20Momo%20-%20N3ichou%20la%20vida.mp3"
   },
   {
@@ -56,7 +56,7 @@ export default {
     "name": "Aghach",
     "artistName": "LFERDA",
     "year": 2020,
-    "src": "./assets/lferda.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/lferda.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/LFERDA%20-%20Aghach.mp3"
   },
   {
@@ -64,7 +64,7 @@ export default {
     "name": "Koula Nhar",
     "artistName": "Lord Mehdi",
     "year": 2020,
-    "src": "./assets/lord mehdi.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/lord mehdi.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/Lord%20Mehdi%20-%20Koula%20Nhar.mp3"
   },
   {
@@ -72,7 +72,7 @@ export default {
     "name": "Tant pis",
     "artistName": "Lord Mehdi",
     "year": 2020,
-    "src": "./assets/lord mehdi.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/lord mehdi.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/Lord%20Mehdi%20-%20Tant%20pis.mp3"
   },
   {
@@ -80,7 +80,7 @@ export default {
     "name": "Foutni W 9NI",
     "artistName": "Nessyou",
     "year": 2020,
-    "src": "./assets/nessyou.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/nessyou.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/Nessyou%20-%20Foutni%20w%209ni.mp3"
   },
   {
@@ -88,7 +88,7 @@ export default {
     "name": "DRAIL",
     "artistName": "MA3iZ",
     "year": 2020,
-    "src": "./assets/ma3iz.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/ma3iz.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/MA3IZ%20-%20DRAILL.mp3"
   },
   {
@@ -96,7 +96,7 @@ export default {
     "name": "JRO7i",
     "artistName": "MA3iZ",
     "year": 2020,
-    "src": "./assets/ma3iz.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/ma3iz.jpg",
     "songSrc": "https://xoom.virgilio.it/source_filemanager/ws/ss/wss/MA3IZ%20-%20JRO7I.mp3"
   },
   {
@@ -104,7 +104,7 @@ export default {
     "name": "Adams Ft. Raid",
     "artistName": "Lmorphine",
     "year": 2020,
-    "src": "./assets/lmorphine.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/lmorphine.jpg",
     "songSrc": "https://wss.xoom.it/inkonnu/L%20morphine%20-%20Adams%2C%20Raid.mp3"
   },
   {
@@ -112,7 +112,7 @@ export default {
     "name": "Sparadra",
     "artistName": "Lmorphine",
     "year": 2020,
-    "src": "./assets/lmorphine.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/lmorphine.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/inkonnu/L%20morphine%20-%20Sparadra.mp3"
   },
   {
@@ -120,7 +120,7 @@ export default {
     "name": "Tab3ini V3",
     "artistName": "Inkonnu",
     "year": 2020,
-    "src": "./assets/inkonnu.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/inkonnu.jpg",
     "songSrc": "https://wss.xoom.it/inkonnu/Inkonnu%20-%20Tab3ini%20V3.mp3"
   },
   {
@@ -128,7 +128,7 @@ export default {
     "name": "RIP LOVE",
     "artistName": "Faouzia",
     "year": 2020,
-    "src": "./assets/faouzia.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/faouzia.jpg",
     "songSrc": "http://xoom.virgilio.it/source_filemanager/ws/ss/wss/Faouzia%20-%20RIP%2C%20Love.mp3"
   },
   {
@@ -136,7 +136,7 @@ export default {
     "name": "Breda",
     "artistName": "Inkonnu",
     "year": 2020,
-    "src": "./assets/inkonnu.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/inkonnu.jpg",
     "songSrc": "https://xoom.virgilio.it/source_filemanager/ws/ss/wss/inkonnu/Inkonnu%20-%20BREDA.mp3"
   },
   {
@@ -144,7 +144,7 @@ export default {
     "name": "Chill",
     "artistName": "Inkonnu",
     "year": 2020,
-    "src": "./assets/inkonnu.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/inkonnu.jpg",
     "songSrc": "https://xoom.virgilio.it/source_filemanager/ws/ss/wss/inkonnu/Inkonnu%20-%20Chill.mp3"
   },
   {
@@ -152,7 +152,7 @@ export default {
     "name": "Matebkich",
     "artistName": "Klass-A",
     "year": 2020,
-    "src": "./assets/klass-a.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/klass-a.jpg",
     "songSrc": "https://xoom.virgilio.it/source_filemanager/ws/ss/wss/inkonnu/Klass-A%20-%20Matebkich.mp3"
   },
   {
@@ -160,7 +160,7 @@ export default {
     "name": "After Prophets Life",
     "artistName": "Nostik",
     "year": 2020,
-    "src": "./assets/nostik.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/nostik.jpg",
     "songSrc": "https://xoom.virgilio.it/source_filemanager/ws/ss/wss/Nostik%20-%20After%20Prophets%20Life.mp3"
   },
   {
@@ -168,7 +168,7 @@ export default {
     "name": "Aladin",
     "artistName": "Nostik",
     "year": 2020,
-    "src": "./assets/nostik.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/nostik.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/JruOYyXK/54cabd1f-ab5b-4c39-9c87-50f79f6ed34d.mp3"
   },
   {
@@ -176,7 +176,7 @@ export default {
     "name": "Lbaroud Ft. Inkonnu",
     "artistName": "Draganov",
     "year": 2020,
-    "src": "./assets/draganov.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/draganov.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/kpun0qBY/a3256b56-a70d-4582-8ce7-be6201c7fb8f.mp3"
   },
   {
@@ -184,7 +184,7 @@ export default {
     "name": "Comite",
     "artistName": "Draganov",
     "year": 2020,
-    "src": "./assets/draganov.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/draganov.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/qGuRjLAY/56cef9e1-f40c-4588-9595-884f26349103.mp3"
   },
   {
@@ -192,7 +192,7 @@ export default {
     "name": "Ma Cherie",
     "artistName": "Naika",
     "year": 2020,
-    "src": "./assets/naika.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/naika.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/DOu6XyDx/c5154877-3454-4e57-bd5b-cfe08bd322ac.mp3"
   },
   {
@@ -200,7 +200,7 @@ export default {
     "name": "Kssiriri",
     "artistName": "Draganov",
     "year": 2020,
-    "src": "./assets/draganov.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/draganov.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/ApuEB2wO/c63faea2-a58e-46d5-b071-48d5432a8330.mp3"
   },
   {
@@ -208,7 +208,7 @@ export default {
     "name": "Table 42",
     "artistName": "Klass-A",
     "year": 2020,
-    "src": "./assets/klass-a.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/klass-a.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/geuA4Qn5/41f9c549-8c8e-4fee-895a-22f937ff021f.mp3"
   },
   {
@@ -216,7 +216,7 @@ export default {
     "name": "Cellule Ft. Raid",
     "artistName": "Diib",
     "year": 2020,
-    "src": "./assets/diib.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/diib.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/12uvoq58/5fe82507-0478-49b2-a194-9ad23494e9fc.mp3"
   },
   {
@@ -224,7 +224,7 @@ export default {
     "name": "Prince Nassim",
     "artistName": "Hamza15-3",
     "year": 2020,
-    "src": "./assets/hamza15-3.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/hamza15-3.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/Jrux1lQe/5db26c0a-470b-404d-a782-ce92d50265d6.mp3"
   },
   {
@@ -232,7 +232,7 @@ export default {
     "name": "Denya",
     "artistName": "Klass-A",
     "year": 2020,
-    "src": "./assets/klass-a.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/klass-a.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/P8uGY1g0/794bd993-b5d6-47a4-aebc-bfca989babe8.mp3"
   },
   {
@@ -240,7 +240,7 @@ export default {
     "name": "Flashback",
     "artistName": "Vargas",
     "year": 2020,
-    "src": "./assets/vargas.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/vargas.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/d5uReANg/6e14fd57-bda4-48a2-abcb-9ab4fc5c962f.mp3"
   },
   {
@@ -248,7 +248,7 @@ export default {
     "name": "DNT Ft. Nouvo",
     "artistName": "Draganov",
     "year": 2020,
-    "src": "./assets/draganov.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/draganov.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/lluojlA0/d5845ba6-ea54-4e5b-a296-f7983dae7329.flac"
   },
   {
@@ -256,7 +256,7 @@ export default {
     "name": "Youm Wara Youm",
     "artistName": "Stormy",
     "year": 2020,
-    "src": "./assets/stormy.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/stormy.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/mXuKdXQp/74059561-128e-48e2-b5dc-cedb8a8a72d3.mp3"
   },
   {
@@ -264,7 +264,7 @@ export default {
     "name": "Hkaya",
     "artistName": "SNOR",
     "year": 2020,
-    "src": "./assets/snor.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/snor.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/L1uBA0L0/2c576c7f-ee26-4f58-84e5-b21c65fd7486.mp3"
   },
   {
@@ -272,7 +272,7 @@ export default {
     "name": "Bonne Année",
     "artistName": "Tawsen",
     "year": 2020,
-    "src": "./assets/tawsen.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/tawsen.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/z8uOgdmL/68bcc597-fe88-42f3-a63b-1ee1514fbd25.mp3"
   },
   {
@@ -280,7 +280,7 @@ export default {
     "name": "Omri Ana Ft. Tagne",
     "artistName": "Stormy",
     "year": 2020,
-    "src": "./assets/stormy.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/stormy.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/JruDOE8X/2aff8d7b-1ed3-4fa3-a4c6-f6f7e4082ec6.mp3"
   },
   {
@@ -288,7 +288,7 @@ export default {
     "name": "Tri9 Lil Ft. Gha4",
     "artistName": "Inkonnu",
     "year": 2020,
-    "src": "./assets/inkonnu.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/inkonnu.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/kpuDJZnE/c19ff085-c55b-4d8f-9889-f77f5a3e71ab.mp3"
   },
   {
@@ -296,7 +296,7 @@ export default {
     "name": "Shredder",
     "artistName": "Diib",
     "year": 2020,
-    "src": "./assets/diib.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/diib.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/rRujJ2on/16ba29e5-ae1d-4f6b-8847-a50a103ca49d.flac"
   },
   {
@@ -304,7 +304,7 @@ export default {
     "name": "N",
     "artistName": "Madd",
     "year": 2020,
-    "src": "./assets/madd.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/madd.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/wbu6j5D2/e11adcff-179e-4383-8f85-38248c234c53.mp3"
   },
   {
@@ -312,7 +312,7 @@ export default {
     "name": "Peek.A.Boo",
     "artistName": "Pause",
     "year": 2020,
-    "src": "./assets/pause.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/pause.jpg",
     "songSrc": "https://p9.f1.n0.cdn.getcloudapp.com/items/Blu4nw8E/9e9b4361-46bc-46bf-9a7f-95aaf50cbd4b.mp3"
   },
   {
@@ -320,7 +320,7 @@ export default {
     "name": "Tt Va Bien",
     "artistName": "Draganov",
     "year": 2020,
-    "src": "./assets/draganov.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/draganov.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/4gu2odqL/77c1ba15-fa28-47c7-a5e3-ab3972b8ac73.mp3"
   },
   {
@@ -328,7 +328,7 @@ export default {
     "name": "Memo",
     "artistName": "Furelise",
     "year": 2020,
-    "src": "./assets/furelise.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/furelise.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/Z4uj6xk5/2e8ff242-7cfc-48f5-b9a5-83f5cd67d3a1.mp3"
   },
   {
@@ -336,7 +336,7 @@ export default {
     "name": "Lili",
     "artistName": "Plylist",
     "year": 2020,
-    "src": "./assets/plylist.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/plylist.jpg",
     "songSrc": "https://p9.f1.n0.cdn.getcloudapp.com/items/llu5WmNq/5955d479-c49f-45b5-bfc5-737214510edd.mp3"
   },
   {
@@ -344,7 +344,7 @@ export default {
     "name": "ALOHA",
     "artistName": "ElGrandeToto",
     "year": 2020,
-    "src": "./assets/elgrandetoto.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/elgrandetoto.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/RBuYA5QP/e27eb3e0-13e8-4931-a2eb-a1d677aa186b.mp3"
   },
   {
@@ -352,7 +352,7 @@ export default {
     "name": "Morose",
     "artistName": "Damso",
     "year": 2020,
-    "src": "./assets/damso.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/damso.jpg",
     "songSrc": "https://p52.f0.n0.cdn.getcloudapp.com/items/xQubRrx5/16a5093a-fda3-4559-a3ef-ea66f0a188fc.mp3"
   },
   {
@@ -360,7 +360,7 @@ export default {
     "name": "Pegasus Ft. Raid & Diib",
     "artistName": "Mehdi Black Wind",
     "year": 2020,
-    "src": "./assets/mehdi black wind.jpg",
+    "src": "https://raw.githubusercontent.com/DrissBouda/vuejs-music-app/master/src/assets/mehdi black wind.jpg",
     "songSrc": "https://drissbouda.xoom.it/mp3/Mehdi%20Black%20Wind%20-%20PEGASUS%20(feat.%20Diib%20&%20Raid).mp3"
   }
 ]
